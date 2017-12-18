5c98eebe046b0f1fcec2a27089206e1b733c8ccc
kutyel <flaviocorpa@gmail.com>
2017-12-18 17:07:24 +0100

    First implementation

--- index.js
+++ index.js
@@ -10,15 +10,32 @@ const bot = new Twit({
   timeout_ms: 60 * 1000,
 })
 
-const getUsers = async () => {
+const sleep = ms => new Promise((resolve, reject) => setTimeout(resolve, ms))
+
+const diff = (old, news) => news.filter(n => !old.includes(n))
+
+const getUsers = async (all = [], cursor = -1) => {
   try {
-    const { data: { users } } = await bot.get('followers/list', { count: 200 })
-    if (users.length) {
-      users.forEach((user, i) => console.log(i + 1, user.screen_name))
-    }
+    const { data: { users = [], next_cursor_str: nextCursor } } = await bot.get('followers/list', {
+      count: 200,
+      cursor,
+    })
+    all = [...all, ...users.map(x => x.screen_name)]
+    return nextCursor && nextCursor === '0' ? all : getUsers(all, nextCursor)
   } catch (err) {
     console.error(err)
+    return []
   }
 }
 
-getUsers()
+const main = async () => {
+  const oldFollowers = await getUsers()
+  console.log('oldFollowers', oldFollowers)
+  await sleep(15 * 60 * 1000)
+  const currentFollowers = await getUsers()
+  console.log('currentFollowers', currentFollowers)
+  const unfollows = diff(oldFollowers, currentFollowers)
+  unfollows.forEach(user => console.log(`User @${user} stopped following you!`))
+}
+
+main()
