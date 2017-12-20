3ca5e79e58b2d60e46d7c7730cba076aaad83f3f
kutyel <flaviocorpa@gmail.com>
2017-12-20 10:36:37 +0100

    Fix standard styling 💅🏼

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const bot = new Twit({
   consumer_secret: process.env.CONSUMER_SECRET,
   access_token: process.env.ACCESS_TOKEN,
   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-  timeout_ms: 60 * 1000,
+  timeout_ms: 60 * 1000
 })
 
 const sleep = ms => new Promise((resolve, reject) => setTimeout(resolve, ms))
@@ -18,7 +18,7 @@ const getUsers = async (all = [], cursor = -1) => {
   try {
     const { data: { users = [], next_cursor_str: nextCursor } } = await bot.get('followers/list', {
       count: 200,
-      cursor,
+      cursor
     })
     all = [...all, ...users.map(x => x.screen_name)]
     return nextCursor && nextCursor === '0' ? all : getUsers(all, nextCursor)
@@ -36,7 +36,7 @@ const main = async () => {
   unfollows.forEach(user =>
     bot.post('direct_messages/new', {
       screen_name: process.env.TWITTER_HANDLER,
-      text: `User @${user} stopped following you!`,
+      text: `User @${user} stopped following you!`
     })
   )
 }
