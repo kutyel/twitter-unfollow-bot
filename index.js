a3596dcd8a312097329a46e5854ce0f20432cd90
kutyel <flaviocorpa@gmail.com>
2017-12-21 11:44:53 +0100

    Encapsulate into module

--- index.js
+++ index.js
@@ -11,34 +11,36 @@ const bot = new Twit({
   timeout_ms: 60 * 1000
 })
 
-const diff = xs => xy => xs.filter(x => !xy.includes(x))
-const sleep = ms => new Task((rej, res) => setTimeout(res, ms))
+module.exports = () => {
+  const diff = xs => xy => xs.filter(x => !xy.includes(x))
+  const sleep = ms => new Task((rej, res) => setTimeout(res, ms))
 
-const get = (url, opts = { count: 200 }) =>
-  new Task((rej, res) =>
-    bot.get(url, opts, (err, data, response) => (err ? rej(err) : res(data)))
-  )
+  const get = (url, opts = { count: 200 }) =>
+    new Task((rej, res) =>
+      bot.get(url, opts, (err, data, response) => (err ? rej(err) : res(data)))
+    )
 
-const notify = users =>
-  users.map(user =>
-    bot.post('direct_messages/new', {
-      screen_name: process.env.TWITTER_HANDLER,
-      text: `User @${user} stopped following you!`
-    })
-  )
+  const notify = users =>
+    users.map(user =>
+      bot.post('direct_messages/new', {
+        screen_name: process.env.TWITTER_HANDLER,
+        text: `User @${user} stopped following you!`
+      })
+    )
 
-const getFollowers = (all = [], cursor = 0) =>
-  get('followers/list', { count: 200, cursor }).chain(
-    ({ users, next_cursor_str: nextCursor }) =>
-      nextCursor && nextCursor === '0'
-        ? Task.of(all.concat(users).map(user => user.screen_name))
-        : getFollowers(all.concat(users), nextCursor)
-  )
+  const getFollowers = (all = [], cursor = 0) =>
+    get('followers/list', { count: 200, cursor }).chain(
+      ({ users, next_cursor_str: nextCursor }) =>
+        nextCursor && nextCursor === '0'
+          ? Task.of(all.concat(users).map(user => user.screen_name))
+          : getFollowers(all.concat(users), nextCursor)
+    )
 
-const main = () =>
-  Task.of(diff)
-    .ap(getFollowers())
-    .ap(sleep(15 * 60 * 1000).chain(getFollowers))
-    .fork(console.error, notify)
+  const main = () =>
+    Task.of(diff)
+      .ap(getFollowers())
+      .ap(sleep(15 * 60 * 1000).chain(getFollowers))
+      .fork(console.error, notify)
 
-module.exports = () => setInterval(main, 15 * 60 * 1000)
+  setInterval(main, 15 * 60 * 1000)
+}
